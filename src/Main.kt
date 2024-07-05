e21d132ffa79ca44ba2baf790561c09269220e8f
PawPrintsInTheDark <an.kyt.ot15@gmail.com>
2024-07-05 19:33:48 +0300

    init Project

---
+++ src/Main.kt
@@ -0,0 +1,8 @@
+fun main() {
+    val age = 20
+    val name = "Андрей"
+    val isStudent = true
+    val salary: Double = 1_000_000.0
+
+    println("Age: $age | Name: $name | Is Student: $isStudent | Salary: $salary")
+}
