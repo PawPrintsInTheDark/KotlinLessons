3fd01ae0d97f2718ebac1fb7f110401bcfea9866
PawPrintsInTheDark <an.kyt.ot15@gmail.com>
2024-07-07 00:27:11 +0300

    add Оператор If solution

--- src/Main.kt
+++ src/Main.kt
@@ -1,24 +1,43 @@
 const val PI = 3.14
 fun main() {
-    val a = 20
-    val b = 10
-    val d = 90
-    val L = 658
-    val A = 243
-    val B = 25
-
-    var P = a * 4
-    var S = a * A
-    println("Квадрат: P = $P | S = $S")
-
-    P = 2 * (a + b)
-    S = a*b
-    println("Прямоугольник: P = $P | S = $S ")
-
-    println("Окружность: L = " + PI*d)
-
-    println("Кол-во полных метров в растоянии $L: "+ L/100+" m")
-    println("Кол-во отрезков В, размещенных на отрезке А: "+ A/B)
-
-
+    // 1.
+    var a = 20
+    if (a > 0) {
+        a++
+        println(a)
+    }
+    // 2.
+    var i = 0
+    if (4 > 0) i++
+    if (45 > 0) i++
+    if (-2 > 0) i++
+    if (8 > 0) i++
+    println("Кол-во положительных чисел: $i")
+    // 3.
+    val c = 20
+    val d = 34
+    println(if (c > d) c else d)
+    // 4.
+    val num = 7
+    println(
+        if (num == 1) "Понедельник" else if (num == 2) "Вторник"
+        else if (num == 3) "Среда" else if (num == 4) "Четверг"
+        else if (num == 5) "Пятница" else if (num == 6) "Суббота"
+        else "Воскресенье"
+    )
+    // 5.
+    val R = 43
+    println(
+        if (R == 1) "Плохо" else if (R == 2) "Неудовлетворительно"
+        else if (R == 3) "Удовлетворительно" else if (R == 4) "Хорошо"
+        else if (R == 5) "Отлично" else "Ошибка"
+    )
+    // 6.
+    val action = 1
+    val e = 3.2
+    val t = 45.4
+    println(
+        if (action == 1) e+t else if (action == 2) a-t
+        else if (action == 3) e*t else e/t
+    )
 }
