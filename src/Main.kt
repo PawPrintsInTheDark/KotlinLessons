57451ca9ea3fcdcb507cf39c675be61ec053485a
PawPrintsInTheDark <an.kyt.ot15@gmail.com>
2024-07-08 11:19:53 +0300

    add While, Do-while solution

--- src/Main.kt
+++ src/Main.kt
@@ -1,66 +1,54 @@
 fun main() {
-    // 1.
-    //    val mounth = readln().toInt()
-    //    println("Введите номер месяца:")
-    when (val mounth = 1) {
-        12, 1, 2 -> {
-            print("Зима: ")
-            when (mounth) {
-                12 -> println("Декабрь")
-                1 -> println("Январь")
-                2 -> println("Февраль")
-            }
-        }
-
-        3, 4, 5 -> {
-            print("Весна: ")
-            when (mounth) {
-                3 -> println("Март")
-                4 -> println("Апрель")
-                5 -> println("Май")
-            }
-        }
-
-        6, 7, 8 -> {
-            print("Лето: ")
-            when (mounth) {
-                6 -> println("Июнь")
-                7 -> println("Июль")
-                8 -> println("Август")
-            }
-        }
-
-        9, 10, 11 -> {
-            print("Осень: ")
-            when (mounth) {
-                9 -> println("Сентябрь")
-                10 -> println("Октябрь")
-                11 -> println("Ноябрь")
-            }
-        }
-
-        else -> println("Ошибка, такого месяца не существует")
-    }
-    // 2.
-    val num: Double = 34.54675
-    val res = when (num % 1) {
-        in 0.1..0.4 -> num.toInt()
-        in 0.5..0.9 -> num.toInt() + 1
-        else -> num
-    }
-    println(res)
-    // 3.
-    val time = 18
-    when (time) {
-        in 0..5 -> println("Раннее утро")
-        in 6..11 -> println("Утро")
-        in 12..17 -> println("День")
-        in 18..20 -> println("Вечер")
-        in 21..23 -> println("Поздний вечер")
-    }
+    //TODO прошлое невыполненное задание по оператору when
     // 1.4 Даны две переменные a = 8, b = 5. Написать алгоритм, при котором a = 5, b = 8.
-    // ???
-    val a = 5;
-    val b = 8
+    var a = 8
+    var b = 5
+    println("Исходные значения: а = $a, b = $b")
+    // 1 способ
+    a = a xor b // можно так же, но через - и +
+    b = a xor b
+    a = a xor b
+    println("После перестановки: а = $a, b = $b")
+    // 2 способ
+    a = b.also { b = a }
 
+    println("После перестановки: а = $a, b = $b")
+    // TODO While, Do-while
+    // 1.
+    println("Введите число:")
+    var num = readln().toInt()
+    var res = 1
+    while (num != 1) res *= num--
+    println("Факториал числа = $res")
+    // 2.
+    val count = 10
+    val price = 77
+    var i = 1
+    while (i <= count) println("Стоимость $i кг конфет: ${price * i++} руб.")
+    // 3.
+    var A = 10
+    val B = 32
+    println("Кол-во чисел: ${B-A}")
+    while (A <= B) print("${A++} ")
+    // 4. Сначала не понял, а потом как понял
+    var N = 12345
+    println("")
+    while (N > 0){
+        print("${N % 10}")
+        N /= 10
+    }
+    // 5.
+    println("")
+    var f = 1
+    while (f++ < 20 ) if (f == 19) break else if (f % 2 == 0) if (f % 4 == 0) print("${f * 2} ") else print("$f ")
+    // 6.
+    println("")
+    var deposit = 350.0
+    var month = 9
+    while (month-- >= 1){
+        deposit += deposit * 0.07
+    }
+    println("Конечная сумма вклада = %.2f".format(deposit))
 }
+
+
