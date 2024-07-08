5490d1277003770cbaa3585d7b81bc096ddd8bd5
PawPrintsInTheDark <an.kyt.ot15@gmail.com>
2024-07-08 17:38:29 +0300

    add Оператор For solution

--- src/Main.kt
+++ src/Main.kt
@@ -1,54 +1,41 @@
-fun main() {
-    //TODO прошлое невыполненное задание по оператору when
-    // 1.4 Даны две переменные a = 8, b = 5. Написать алгоритм, при котором a = 5, b = 8.
-    var a = 8
-    var b = 5
-    println("Исходные значения: а = $a, b = $b")
-    // 1 способ
-    a = a xor b // можно так же, но через - и +
-    b = a xor b
-    a = a xor b
-    println("После перестановки: а = $a, b = $b")
-    // 2 способ
-    a = b.also { b = a }
+import kotlin.math.abs
+import kotlin.math.pow
+import kotlin.math.sqrt
 
-    println("После перестановки: а = $a, b = $b")
-    // TODO While, Do-while
+const val pi = 3.14
+fun main() {
     // 1.
-    println("Введите число:")
-    var num = readln().toInt()
-    var res = 1
-    while (num != 1) res *= num--
-    println("Факториал числа = $res")
+    var a = 213
+    var b = 190.0
+    if (a < b) while (++a < --b); else while (++b < --a); // можно так
+    println(b)
+    println("Cреднее арифметическое равно ${(a + b) / 2.0}") // но этот способ короче и точнее
     // 2.
-    val count = 10
-    val price = 77
-    var i = 1
-    while (i <= count) println("Стоимость $i кг конфет: ${price * i++} руб.")
+    val S = 50.24
+    val D = sqrt(4 * S / pi)
+    val L = pi * D
+    println("Диаметр окружности: $D")
+    println("Длина окружности: $L")
     // 3.
-    var A = 10
-    val B = 32
-    println("Кол-во чисел: ${B-A}")
-    while (A <= B) print("${A++} ")
-    // 4. Сначала не понял, а потом как понял
-    var N = 12345
-    println("")
-    while (N > 0){
-        print("${N % 10}")
-        N /= 10
+    val x1 = 1
+    val y1 = 6
+    val x2 = 8
+    val y2 = 1
+    val P = 2 * (abs(x2 - x1) + abs(y2 - y1))
+    val Sp = abs(x2 - x1) * abs(y2 - y1)
+    println("Периметр прямоугольника: $P")
+    println("Площадь прямоугольника: $Sp")
+    // 4.
+    for (i in 0..9) {
+        print("${2.0.pow(i).toInt()} ")
     }
     // 5.
     println("")
-    var f = 1
-    while (f++ < 20 ) if (f == 19) break else if (f % 2 == 0) if (f % 4 == 0) print("${f * 2} ") else print("$f ")
-    // 6.
-    println("")
-    var deposit = 350.0
-    var month = 9
-    while (month-- >= 1){
-        deposit += deposit * 0.07
+    var n = 20
+    while (n >= 0) {
+        if(n % 2 == 0) println(n)
+        n--
     }
-    println("Конечная сумма вклада = %.2f".format(deposit))
 }
 
 
