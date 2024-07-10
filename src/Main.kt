d667a09511b9c20fccdbea2b53f6c9eaa576b619
PawPrintsInTheDark <an.kyt.ot15@gmail.com>
2024-07-10 18:22:28 +0300

    add Работа с массивами solution

--- src/Main.kt
+++ src/Main.kt
@@ -2,55 +2,24 @@ import kotlin.math.pow
 
 fun main() {
     // 1.
-    val num = 12
-    var res = 0.0
-    for (t in 1..num){
-        res += 1.0 / t
-    }
-    println("%.2f".format(res))
+    val arr = Array(10) { i -> i + 0.25 }
+    println("array 1 = ${arr.contentToString()}")
+
+    println("Произведение элементов массива: ${arr.reduce { i, j -> i * j }}")
     // 2.
-    var a = 2
-    val b = 12
-    var res1 = 0
-    while (a <= b) {
-        res1 += a.toDouble().pow(2.0).toInt()
-        a++
-    }
-    println("Cумма квадратов = $res1")
+    val arr2 = Array(8) { i -> (i+1) * (i+1) }
+    println("array 2 = ${arr2.contentToString()}")
+    println("Среднее арифметическое массива: ${arr2.average()}")
     // 3.
-    val A = 3
-    val B = 8
-    var d = 0
-    for (t in A..B) {
-        repeat(d) { print("${t}, ") }
-        d++
-    }
+    val size = 15
+    val arr3 = Array(size) { i -> i + 1 }
+    println("array 3 = ${arr3.contentToString()}")
+    val filterArr = arr3.filter { it % 2 == 0 }
+    println("Все четные числа: $filterArr\nИ их кол-во: ${filterArr.size}")
     // 4.
-    println("")
-    val isSimple = 11
-    if (isSimple <= 1) println("Число $isSimple не простое")
-    else if (isSimple == 2) println("Число $isSimple простое")
-    else if (isSimple % 2 == 0) println("Число $isSimple не простое ")
-    else {
-        var y = 3
-        while (y * y <= isSimple) {
-            if (isSimple % y == 0) return println("$isSimple не простое")
-            y += 2
-        }
-        println("Число $isSimple простое")
-    }
-    // 5.
-    println("Что это такое: синий, большой, с усами и полностью набит зайцами?")
-    while (true) {
-        val str = readln()
-        if (str.equals("Сдаюсь", true)) {
-            println("Правильный ответ: троллейбус.")
-            return
-        } else if (str.equals("Троллейбус", true)) {
-            println("Правильно!")
-            return
-        } else println("Подумай еще.")
-    }
+    val n = 12
+    val arr4 = Array(n) { i -> 2.0.pow(i + 1).toInt() }
+    println("array 4 состоящий из степени 2 = ${arr4.contentToString()}")
 }
 
 
