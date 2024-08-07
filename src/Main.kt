5bc8178de5153acab9e602a6541a08b561924b69
PawPrintsInTheDark <an.kyt.ot15@gmail.com>
2024-08-08 02:20:44 +0300

    add string class solution

--- src/Main.kt
+++ src/Main.kt
@@ -1,29 +1,18 @@
 fun main() {
     // 1.
-    repeatPrint(3) { println("Привет Мир!") }
-    // 2.
-line("=")
-    val a = arrayOf(1, 2, 3, 4, 5)
-    val arithmeticMean = { arr: Array<Int> -> println("Cреднее арифметическое: "+ arr.sum().toDouble() / arr.size) }
-    arithmeticMean(a)
-    // 3.
-line("=")
-    val arr = arrayOf(1, -2, 3, 5, -67, 78, -9, 0, 24, 2, 83, -3)
-    println(arr.filter { it > 0 })
-    // 4.
-line("=")
-    val arr2 = arrayOf(10, 43, 12, 54, 23, 6, 1, 98, 1, 8)
-    val processArr = { array: Array<Int> -> array.map { if (it % 2 == 0) it / 2 else it * 5 } }
-    println(processArr(arr2))
-}
-
-fun repeatPrint(count: Int, print: () -> Unit) {
-    repeat(count) {
-        print()
-    }
-}
-
-fun line(s : String){
-    repeat(70){ print(s)}
+    val str = "Занятие"
+    str.forEach { print("$it ") }
     println()
+    // 2.
+    val num = "+79992245491"
+    println(checkNum(num))
+    // 3.
+    val str2 = "Привет Мир!"
+    println(str2.reversed())
+    // 4.
+    val str3 = "kotlin2023course"
+    println(str3.count { it.isDigit() })
+}
+fun checkNum(num: String): Boolean {
+    return if (num.startsWith("+7") && num.length == 12 && num.all { it.isDigit() }) return true else false
 }
