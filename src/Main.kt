3f75f2312bbdc2b00cb36cfb2d301921c47983db
Cat <an.kyt.ot15@gmail.com>
2024-08-14 01:13:12 +0300

    Преобразование типов

--- src/Main.kt
+++ src/Main.kt
@@ -1,25 +1,41 @@
 fun main() {
     // 1.
-    val arr = arrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15)
-    val states = States<Int>()
-    println(states.getAverage(arr))
-    // 1.2
-    val arr2 = arrayOf(1, 2, 34, 567, 89, 0)
-    val arr3= arrayOf("one","two","three")
-    val gen = GenericMethod()
-    println(gen.isItIncluded(3, arr2))
-    println(gen.isItIncluded("one", arr3))
-
+    val str = "12.3".toFloat()
+    println(str.toInt())
+    println(str)
+    println(str.toInt().toString(2))
+    // 2.
+    line()
+    fun <T> checkType(c: T): String {
+        return when (c) {
+            is Int -> "Это число"
+            is String -> "Это строка"
+            is Double, Float -> "Это число с плавающей точкой"
+            is Char -> "Это символ"
+            else -> c!!::class.simpleName.toString()
+        }
+        // return "Это тип - "+c!!::class.simpleName.toString()
+    }
+    println(checkType("Это строка"))
+    println(checkType(11))
+    println(checkType(12.2))
+    println(checkType('1'))
+    // 3.
+    line()
+    fun <T> sum(el: T) {
+        when (el){
+            is Int -> println(el+1)
+            is String -> println(el.length +1)
+            is IntArray -> println(el.sum())
+        }
+    }
+    sum(1)
+    sum("123")
+    sum(intArrayOf(1,2,3,4))
 }
 
-class States<T> where T : Number {
-    fun getAverage(arr: Array<T>): Double {
-        return arr.sumOf { it.toDouble() } / arr.size
-    }
-}
 
-class GenericMethod {
-    fun <T> isItIncluded(el: T, arr: Array<T>): Boolean {
-        return arr.contains(el)
-    }
+fun line(){
+    repeat(60) { print("=") }
+    println()
 }
