004deffc6624dc0a088d332c1f721bbc3de2022b
Cat <an.kyt.ot15@gmail.com>
2024-08-20 14:39:49 +0300

    CheckAndGroup

--- src/Main.kt
+++ src/Main.kt
@@ -1,24 +1,25 @@
 fun main() {
     // 1.
-    val map = mapOf(1 to "Дима", 2 to "Лёша", 3 to "Анна", 4 to "Юра", 5 to "Андрей", 6 to "Гена")
-    println(map.filter { it.key < 5 })
+    val words = listOf("банан", "яблоко", "апельсин", "груша", "киви", "брусника", "гранат")
+    words.groupBy { it.first() }
+        .forEach { println("Первый символ ${it.key}: ${it.value}") }
     line()
     // 2.
-    println(map.filter { it.value.endsWith("а") })
+    words.groupBy { it.length }
+        .forEach { println("Длина строки ${it.key}: ${it.value}") }
     line()
     // 3.
-    println(map.filter { it.value.startsWith("Г") })
-    line()
-    // 4.
-    val prizeMap = mapOf(
-        15 to 25,
-        20 to 15,
-        25 to 30,
-        35 to 10,
-        40 to 22,
-        55 to 18
+    val phoneToYear = listOf(
+        "Nexus One" to 2010,
+        "Pixel 2" to 2017,
+        "Pixel 4a" to 2020,
+        "iPhone 4" to 2010,
+        "iPhone X" to 2017,
+        "Galaxy Note 8" to 2017,
+        "Galaxy S11" to 2020
     )
-    println(prizeMap.filter { it.value > 20 && it.key % 10 == 5})
+    phoneToYear.groupBy { it.second }
+        .forEach { println("Год ${it.key}: ${it.value.map { it.first }}") }
 }
 
 fun line() {
