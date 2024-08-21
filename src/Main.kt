bfeaa9963599c20c737f92516106bfd945a1da12
Cat <an.kyt.ot15@gmail.com>
2024-08-21 15:14:24 +0300

    Добавил проверку и обработчик на отрицательные числа в sqrt

--- src/Main.kt
+++ src/Main.kt
@@ -7,12 +7,18 @@ fun main() {
     println(str.concat(" World"))
     // 3.
     println(str.lastIndex())
-    // println(str.lastIndex) //TODO зачем писать функцию расширения если уже есть готовое решение?
     // 4.
     println((-1).sqrt())
+
 }
 
 fun Int.pow() = this * this
 fun String.concat(s: String) = this + s
 fun String.lastIndex() = this.lastIndex
-fun Int.sqrt() = kotlin.math.sqrt(this.toDouble()).toInt()
+fun Int.sqrt(): Int? {
+    if (this < 0) {
+        println("Ошибка: нельзя вычислить квадратный корень из отрицательного числа.")
+        return null
+    }
+    return kotlin.math.sqrt(this.toDouble()).toInt()
+}
