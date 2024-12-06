ad793e9ab43f463879d9d958eeeac242fc307d11
Cat <an.kyt.ot15@gmail.com>
2024-12-06 08:44:41 +0500

    add CityStore class

--- src/CityStore.kt
+++ src/CityStore.kt
@@ -1,26 +1,26 @@
-class CityStore(private val cityName: String) : Store {
-    private val availablePhones = listOf(IPhone(), Samsung())
-    private val salesStatistics = mutableMapOf<Phone, Int>()
-    private var repairOffered = false
+class CityStore(private val cityName: String, val availablePhones : List<Phone>, private val hasRepairService: Boolean) : Store {
+     private val salesCount = mutableMapOf<Phone, Int>()
+     private var repairOffered = false
 
     override fun sellPhone(phone: Phone) {
-        salesStatistics[phone] = salesStatistics.getOrDefault(phone, 0) + 1
+        salesCount[phone] = salesCount.getOrDefault(phone, 0) + 1
         println("Вы купили ${phone.model} за ${phone.price} рублей.")
     }
 
-    override fun getStatistics() {
-        println("Статистика продаж в магазине $cityName:")
-        for ((phone, count) in salesStatistics) {
-            println("${phone.model}: $count продано.")
-        }
+    override fun getStatistics() : String{
+        val stats = salesCount.map { "${it.key}: ${it.value} продано" }.joinToString("\n")
+        return "Статистика продаж  в магазине $cityName:\n$stats "
     }
 
     override fun offerRepair(): Boolean {
+        if (!hasRepairService) {
+            println("Ремонтная мастерская недоступна в этом магазине.")
+            return false
+        }
         if (!repairOffered) {
             repairOffered = true
             println("Хотите отремонтировать свой телефон? (да/нет)")
-            val response = readLine()
-            return if (response.equals("да", ignoreCase = true)) {
+            return if (readlnOrNull().equals("да", ignoreCase = true)) {
                 println("Ваш телефон отремонтирован.")
                 true
             } else {
