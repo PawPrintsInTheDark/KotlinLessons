0082e6e95d050e36203b9425e8409066214183d9
Cat <an.kyt.ot15@gmail.com>
2024-12-06 18:51:35 +0500

    AnyNothingUnitPairTriple

--- src/Main.kt
+++ src/Main.kt
@@ -1,8 +1,42 @@
-fun main() {
-    val age = 20
-    val name = "Андрей"
-    val isStudent = true
-    val salary: Double = 1_000_000.0
+fun main(){
+//1.
+    val strings = listOf("{([])}", "{([))}", "{{[])}")
+    for (str in strings){
+        println("Строка: $str, валидность: ${isValidBrackets(str)}")
+    }
+    line()
+//2.
+    val array = intArrayOf(1, 2, 3)
+    permutations(array.toList()).forEach{ println(it)}
+}
 
-    println("Age: $age | Name: $name | Is Student: $isStudent | Salary: $salary")
+fun <T> permutations(list: List<T>): List<List<T>> {
+    if (list.isEmpty()) return listOf(emptyList())
+    return list.flatMap { element ->
+        permutations(list.filter { it != element }).map { (listOf(element) + it) }
+    }
+}
+
+
+fun line(){
+    repeat(68){
+        print("=")
+    }
+    println()
+}
+
+fun isValidBrackets(input: String): Boolean {
+    val stack = mutableListOf<Char>()
+    val bracketMap = mapOf('(' to ')', '{' to '}', '[' to ']')
+    for (char in input){
+        when{
+            char in bracketMap.keys -> stack.add(char) // {
+            char in bracketMap.values -> {
+                if (stack.isEmpty() || bracketMap[stack.removeAt(stack.lastIndex)] != char)
+                    return false
+            }
+        }
+    }
+    // Если стек пуст, все скобки были правильно закрыты
+    return stack.isEmpty()
 }
